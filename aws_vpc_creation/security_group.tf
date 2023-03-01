13548a830f6e27f3a44107ca75ca0ef354fddb03
danish-vultara <danish.rehman@vultara.com>
2023-03-01 20:50:04 +0500

    initial commit

---
+++ aws_vpc_creation/security_group.tf
@@ -0,0 +1,27 @@
+resource "aws_security_group" "New_Customer_VPC_SG" {
+  name        = "New_Customer_VPC_SG"
+  description = "security group for New Customer production environment lambda"
+
+  dynamic "ingress" {
+    for_each = var.ports
+    iterator = port
+    content {
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    #ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "New_Customer_VPC_SG"
+  }
+}
