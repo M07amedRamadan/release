fdd9c597a9da58e2b43578edd224fc2f778adf6f
mohamed-mansour8 <145043670+mohamed-mansour8@users.noreply.github.com>
2023-10-03 12:31:02 +0300

    Update and rename default-RT.tf to Private-RT.tf
    fixed the issue with private subnet
    
    Signed-off-by: mohamed-mansour8 <145043670+mohamed-mansour8@users.noreply.github.com>

--- primary_services_code/Private-RT.tf
+++ primary_services_code/Private-RT.tf
@@ -1,7 +1,7 @@
 # No change required in this file
 
-resource "aws_route_table" "default_route_table" {
-  default_route_table_id = aws_vpc.New_Customer_VPC.default_route_table_id
+resource "aws_route_table" "Private_RT" {
+  vpc_id = aws_vpc.New_Customer_VPC.id
 
   route {
     cidr_block     = "0.0.0.0/0"
@@ -17,12 +17,13 @@ resource "aws_route_table" "default_route_table" {
     Name = "${var.CUSTOMER_NAME}-Private_RT"
   }
 }
-resource "aws_route_table_association" "Private_subnet_association_1" {
+
+resource "aws_route_table_association" "private_subnet_association_1" {
   subnet_id      = aws_subnet.private_1.id
-  route_table_id = aws_route_table.default_route_table.id
+  route_table_id = aws_route_table.Private_RT.id
 }
 
-resource "aws_route_table_association" "Private_subnet_association_2" {
+resource "aws_route_table_association" "private_subnet_association_2" {
   subnet_id      = aws_subnet.private_2.id
-  route_table_id = aws_route_table.default_route_table.id
+  route_table_id = aws_route_table.Private_RT.id
 }
