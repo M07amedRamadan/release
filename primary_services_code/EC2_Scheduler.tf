895d4d9d5d4e5ea05c0d32844c3276e8f494c4bd
mohamed-mansour8 <145043670+mohamed-mansour8@users.noreply.github.com>
2023-10-20 15:18:05 +0300

    Update EC2_Scheduler.tf
    Signed-off-by: mohamed-mansour8 <145043670+mohamed-mansour8@users.noreply.github.com>

--- primary_services_code/EC2_Scheduler.tf
+++ primary_services_code/EC2_Scheduler.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "web" {
   key_name               = "${var.key_name}"
   vpc_security_group_ids = [aws_security_group.New_Customer_VPC_SG.id]
   subnet_id              = aws_subnet.private_1.id
-  iam_instance_profile   = "fake2report"
+  iam_instance_profile   = "scheduler-server-role"
   tags = {
     Name = "${var.CUSTOMER_NAME}-report-Prod"
   }
@@ -18,7 +18,7 @@ resource "aws_instance" "web2" {
   key_name               = "${var.key_name}"
   vpc_security_group_ids = [aws_security_group.New_Customer_VPC_SG.id]
   subnet_id              = aws_subnet.public_1.id
-  iam_instance_profile   = "fake2bastion"
+  iam_instance_profile   = "scheduler-server-role"
   tags = {
     Name = "${var.CUSTOMER_NAME}-bastion-host"
   }
