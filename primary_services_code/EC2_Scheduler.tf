8903420d46a28ef090d635ab7aa059008474ae0c
mohamed-mansour8 <145043670+mohamed-mansour8@users.noreply.github.com>
2023-10-20 15:29:19 +0300

    Update EC2_Scheduler.tf
    Signed-off-by: mohamed-mansour8 <145043670+mohamed-mansour8@users.noreply.github.com>

--- primary_services_code/EC2_Scheduler.tf
+++ primary_services_code/EC2_Scheduler.tf
@@ -19,6 +19,7 @@ resource "aws_instance" "web2" {
   vpc_security_group_ids = [aws_security_group.New_Customer_VPC_SG.id]
   subnet_id              = aws_subnet.public_1.id
   iam_instance_profile   = "scheduler-server-role"
+  associate_public_ip_address = true
   tags = {
     Name = "${var.CUSTOMER_NAME}-bastion-host"
   }
