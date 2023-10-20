173ad676e855acc430076a92e6f318d8801b2623
mohamed-mansour8 <145043670+mohamed-mansour8@users.noreply.github.com>
2023-10-20 14:40:28 +0300

    Update EC2_Scheduler.tf
    Signed-off-by: mohamed-mansour8 <145043670+mohamed-mansour8@users.noreply.github.com>

--- primary_services_code/EC2_Scheduler.tf
+++ primary_services_code/EC2_Scheduler.tf
@@ -10,3 +10,16 @@ resource "aws_instance" "web" {
   }
   user_data = file("${path.module}/script.sh")
 }
+
+
+resource "aws_instance" "web" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  key_name               = "${var.key_name}"
+  vpc_security_group_ids = [aws_security_group.New_Customer_VPC_SG.id]
+  subnet_id              = aws_subnet.public_1.id
+  iam_instance_profile   = "scheduler-server-role"
+  tags = {
+    Name = "${var.CUSTOMER_NAME}-bastion-host"
+  }
+}
