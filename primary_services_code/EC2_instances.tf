f6b4b3bf177685e17787146355155c8938791e3c
mohamed-mansour8 <145043670+mohamed-mansour8@users.noreply.github.com>
2023-10-24 23:11:07 +0300

    Update and rename EC2_Scheduler.tf to EC2_instances.tf
    Signed-off-by: mohamed-mansour8 <145043670+mohamed-mansour8@users.noreply.github.com>

--- primary_services_code/EC2_instances.tf
+++ primary_services_code/EC2_instances.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
   ami           = var.ami
   instance_type = var.instance_type
-  key_name               = "vultara-report-server-KP"
+  key_name               = "report-generator-KP"
   vpc_security_group_ids = [aws_security_group.New_Customer_VPC_SG.id]
   subnet_id              = aws_subnet.private_1.id
   iam_instance_profile   = "scheduler-server-role"
