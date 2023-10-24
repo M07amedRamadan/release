3e24b7897a6c5466ad83ad8ec2e670bb4220bc7b
mohamed-mansour8 <145043670+mohamed-mansour8@users.noreply.github.com>
2023-10-24 16:11:04 +0300

    Update EC2_Scheduler.tf
    adding the keys variables
    
    Signed-off-by: mohamed-mansour8 <145043670+mohamed-mansour8@users.noreply.github.com>

--- primary_services_code/EC2_Scheduler.tf
+++ primary_services_code/EC2_Scheduler.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
   ami           = var.ami
   instance_type = var.instance_type
-  key_name               = "${var.key_name}"
+  key_name               = "${var.report_key_name}"
   vpc_security_group_ids = [aws_security_group.New_Customer_VPC_SG.id]
   subnet_id              = aws_subnet.private_1.id
   iam_instance_profile   = "scheduler-server-role"
@@ -12,25 +12,26 @@ resource "aws_instance" "web" {
 }
 
 
-##resource "aws_instance" "web3" {
-##  ami           = var.ami
-##  instance_type = var.instance_type
-##  key_name               = "${var.key_name}"
-##  vpc_security_group_ids = [aws_security_group.New_Customer_VPC_SG.id]
-##  subnet_id              = aws_subnet.private_2.id
-##  iam_instance_profile   = "scheduler-server-role"
-##  tags = {
-##    Name = "${var.CUSTOMER_NAME}-scheduler-Prod"
-##  }
-##  user_data = file("${path.module}/script.sh")
-##}
+resource "aws_instance" "web3" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  key_name               = "${var.scheduler_key_name}"
+  vpc_security_group_ids = [aws_security_group.New_Customer_VPC_SG.id]
+  subnet_id              = aws_subnet.private_2.id
+  iam_instance_profile   = "scheduler-server-role"
+  tags = {
+    Name = "${var.CUSTOMER_NAME}-scheduler-Prod"
+  }
+  user_data = file("${path.module}/script.sh")
+}
+
 
 
 
 resource "aws_instance" "web2" {
   ami           = "t2.micro"
   instance_type = var.instance_type
-  key_name               = "${var.key_name}"
+  key_name               = "${var.bastion_key_name}"
   vpc_security_group_ids = [aws_security_group.New_Customer_VPC_SG.id]
   subnet_id              = aws_subnet.public_1.id
   iam_instance_profile   = "scheduler-server-role"
