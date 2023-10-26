4b97754ddcd9852b1bcce71a8d48d1cc59d87625
mohamed-mansour8 <145043670+mohamed-mansour8@users.noreply.github.com>
2023-10-26 16:07:24 +0300

    Update EC2_instances.tf
    Signed-off-by: mohamed-mansour8 <145043670+mohamed-mansour8@users.noreply.github.com>

--- primary_services_code/EC2_instances.tf
+++ primary_services_code/EC2_instances.tf
@@ -1,15 +1,3 @@
-resource "aws_instance" "report-generator" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  key_name               = "report-generator-KP"
-  vpc_security_group_ids = [aws_security_group.New_Customer_VPC_SG.id]
-  subnet_id              = aws_subnet.private_1.id
-  iam_instance_profile   = "scheduler-server-role"
-  tags = {
-    Name = "${var.CUSTOMER_NAME}-reportgenerator-Prod"
-  }
-  #user_data = file("${path.module}/script.sh")
-}
 
 
 resource "aws_instance" "vultara-scheduler" {
@@ -22,7 +10,7 @@ resource "aws_instance" "vultara-scheduler" {
   tags = {
     Name = "${var.CUSTOMER_NAME}-scheduler-Prod"
   }
-  #user_data = file("${path.module}/script.sh")
+  ###user_data = file("${path.module}/script.sh")
 }
 
 
