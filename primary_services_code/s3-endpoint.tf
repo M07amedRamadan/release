5c37efc5062baed6ebef5dee305e0a2a0b057b60
mohamed-mansour8 <145043670+mohamed-mansour8@users.noreply.github.com>
2023-10-29 22:32:37 +0200

    Update s3-endpoint.tf
    adding docker to the gateway endpoint
    
    Signed-off-by: mohamed-mansour8 <145043670+mohamed-mansour8@users.noreply.github.com>

--- primary_services_code/s3-endpoint.tf
+++ primary_services_code/s3-endpoint.tf
@@ -1,19 +1,54 @@
-resource "aws_vpc_endpoint" "s3" {
+resource "aws_vpc_endpoint" "s3-private" {
   vpc_id       = aws_vpc.New_Customer_VPC.id
+  vpc_endpoint_type = "Gateway"
   service_name = "com.amazonaws.us-east-1.s3"
-  route_table_ids = [ aws_route_table.Public_RT.id, aws_route_table.Private_RT.id ]
+  route_table_ids = [ aws_route_table.Private_RT.id ]
 
-  policy = <<EOF
-{
+}
+
+
+resource "aws_vpc_endpoint" "s3-public" {
+  vpc_id       = aws_vpc.New_Customer_VPC.id
+  vpc_endpoint_type = "Gateway"
+  service_name = "com.amazonaws.us-east-1.s3"
+  route_table_ids = [ aws_route_table.Public_RT.id ]
+
+}
+
+
+resource "aws_vpc_endpoint_policy" "s3-private-policy" {
+  vpc_endpoint_id = aws_vpc_endpoint.s3-private.id
+  policy = jsonencode({
   "Version": "2012-10-17",
   "Statement": [
     {
       "Action": "s3:*",
       "Effect": "Allow",
-      "Resource": [ "arn:aws:s3:::${var.CUSTOMER_NAME}.vultara.com/*","arn:aws:s3:::${var.CUSTOMER_NAME}.vultara.com" ],
+      "Resource": [ "arn:aws:s3:::${var.CUSTOMER_NAME}.vultara.com/*","arn:aws:s3:::${var.CUSTOMER_NAME}.vultara.com",#changed to public when making the new archteture
+                    "arn:aws:s3:::${var.CUSTOMER_NAME}-reports-bucket/*","arn:aws:s3:::${var.CUSTOMER_NAME}-reports-bucket",
+                    "arn:aws:s3:::${var.CUSTOMER_NAME}-cve-cpe-bucket/*","arn:aws:s3:::${var.CUSTOMER_NAME}-cve-cpe-bucket",
+                    "arn:aws:s3:::${var.CUSTOMER_NAME}-help-doc-bucket/*","arn:aws:s3:::${var.CUSTOMER_NAME}-help-doc-bucket" ,
+                     "arn:aws:s3:::docker-images-prod/*","arn:aws:s3:::docker-images-prod"],
       "Principal": "*"
     }
   ]
+})
 }
-EOF
+
+
+resource "aws_vpc_endpoint_policy" "s3-public-policy" {
+  vpc_endpoint_id = aws_vpc_endpoint.s3-public.id
+  policy = jsonencode({
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "s3:*",
+      "Effect": "Allow",
+      "Resource": [ "arn:aws:s3:::${var.CUSTOMER_NAME}-production-file-bucket/*","arn:aws:s3:::${var.CUSTOMER_NAME}-production-file-bucket",
+                     "arn:aws:s3:::docker-images-prod/*","arn:aws:s3:::docker-images-prod" ],
+      "Principal": "*"
+    }
+  ]
+})
 }
+
