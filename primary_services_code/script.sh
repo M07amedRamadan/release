572c50e5f0f8bd350b6a9f42e2ba7a301b5ff3e9
mohamed-mansour8 <145043670+mohamed-mansour8@users.noreply.github.com>
2023-11-01 14:49:18 +0200

    Update script.sh
    Signed-off-by: mohamed-mansour8 <145043670+mohamed-mansour8@users.noreply.github.com>

--- primary_services_code/script.sh
+++ primary_services_code/script.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-apt update -y
+sudo apt-get update -y
 apt install unzip -y
-apt install -y docker.io
+apt-get install -y docker.io
 usermod -aG docker ubuntu
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
-# Start Docker service
-systemctl start docker
-systemctl enable docker
