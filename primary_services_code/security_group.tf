31279474367734c719e6769f1232c147a6306f34
mohamed-mansour8 <145043670+mohamed-mansour8@users.noreply.github.com>
2023-11-06 19:00:14 +0200

    Update security_group.tf
    adding new security groups
    
    Signed-off-by: mohamed-mansour8 <145043670+mohamed-mansour8@users.noreply.github.com>

--- primary_services_code/security_group.tf
+++ primary_services_code/security_group.tf
@@ -59,3 +59,131 @@ resource "aws_default_security_group" "New_Customer_VPC_SG" {
     Name = "${var.CUSTOMER_NAME}-Prod-SG"
   }
 }
+
+
+resource "aws_security_group" "reportGenerator_SG" {
+  name        = "${var.CUSTOMER_NAME}-reportGenerator-SG"
+  description = "security group for New Customer production environment lambda"
+  vpc_id      = aws_vpc.New_Customer_VPC.id 
+
+  ingress {
+    from_port   = 27000
+    to_port     = 28000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "Report generator docker port"
+    from_port        = 4202
+    to_port          = 4202
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.CUSTOMER_NAME}-reportGenerator-Prod-SG"
+  }
+}
+
+
+resource "aws_security_group" "schedulerServer_SG" {
+  name        = "${var.CUSTOMER_NAME}-schedulerServer-SG"
+  description = "security group for New Customer production environment lambda"
+  vpc_id      = aws_vpc.New_Customer_VPC.id 
+
+  ingress {
+    from_port   = 27000
+    to_port     = 28000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "schedulerServer docker port"
+    from_port        = 3000
+    to_port          = 3000
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.CUSTOMER_NAME}-schedulerServer-Prod-SG"
+  }
+}
