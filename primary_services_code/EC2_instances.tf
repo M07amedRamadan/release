17ebf7bbe3f86e4bcf3cecb859857667e38a4e0c
mohamed-mansour8 <145043670+mohamed-mansour8@users.noreply.github.com>
2023-11-06 19:55:28 +0200

    Update EC2_instances.tf
    Signed-off-by: mohamed-mansour8 <145043670+mohamed-mansour8@users.noreply.github.com>

--- primary_services_code/EC2_instances.tf
+++ primary_services_code/EC2_instances.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "report-generator" {
   ami           = var.ami
   instance_type = var.instance_type
-  key_name               = "fake2bastion"
+  key_name               = var.report_key
   vpc_security_group_ids = [aws_default_security_group.reportGenerator_SG.id]
   subnet_id              = aws_subnet.private_1.id
   iam_instance_profile   = "scheduler-server-role"
@@ -15,7 +15,7 @@ resource "aws_instance" "report-generator" {
 resource "aws_instance" "vultara-scheduler" {
   ami           = var.ami
   instance_type = var.instance_type
-  key_name               = "fake2bastion"
+  key_name               = var.scheduler_key
   vpc_security_group_ids = [aws_default_security_group.schedulerServer_SG.id]
   subnet_id              = aws_subnet.private_2.id
   iam_instance_profile   = "scheduler-server-role"
@@ -31,7 +31,7 @@ resource "aws_instance" "vultara-scheduler" {
 resource "aws_instance" "bastion-host" {
   ami           = var.ami
   instance_type = "t2.micro"
-  key_name               = "bastion-host-ssh"
+  key_name               = var.bastion_key
   vpc_security_group_ids = [aws_default_security_group.New_Customer_VPC_SG.id]
   subnet_id              = aws_subnet.public_1.id
   iam_instance_profile   = "scheduler-server-role" 
