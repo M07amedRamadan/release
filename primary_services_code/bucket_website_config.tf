a3844f4bf9aba5bae0b0110fbc4c4de27c8eddc2
mohamed-mansour8 <145043670+mohamed-mansour8@users.noreply.github.com>
2023-12-06 17:50:12 +0200

    Update bucket_website_config.tf
    Signed-off-by: mohamed-mansour8 <145043670+mohamed-mansour8@users.noreply.github.com>

--- primary_services_code/bucket_website_config.tf
+++ primary_services_code/bucket_website_config.tf
@@ -1,9 +1,9 @@
 # No change required in this file
 
-resource "aws_s3_bucket_website_configuration" "New_Customer_website_configuration" {
-  bucket = aws_s3_bucket.New_Customer_Bucket.id
-
-  index_document {
-    suffix = "index.html"
-  }
-}
+#resource "aws_s3_bucket_website_configuration" "New_Customer_website_configuration" {
+#  bucket = aws_s3_bucket.New_Customer_Bucket.id
+#
+#  index_document {
+#    suffix = "index.html"
+#  }
+#}
