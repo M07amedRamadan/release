8f694af369e3c7c499293a02ae04a6c0cb0eb506
mohamed-mansour8 <145043670+mohamed-mansour8@users.noreply.github.com>
2023-12-06 18:18:12 +0200

    Update s3_creation.tf
    Signed-off-by: mohamed-mansour8 <145043670+mohamed-mansour8@users.noreply.github.com>

--- primary_services_code/s3_creation.tf
+++ primary_services_code/s3_creation.tf
@@ -1,26 +1,27 @@
-### No change required in this file
-##
-##resource "aws_s3_bucket" "New_Customer_Bucket" {
-##  bucket = "${var.CUSTOMER_NAME}.vultara.com"
-##  region = var.region
-##  force_destroy = true
-##}
-##
-###Public Access
-##resource "aws_s3_bucket_public_access_block" "New_Customer_Bucket_Permissions_Block" {
-##  bucket = aws_s3_bucket.New_Customer_Bucket.bucket
-##
-##  block_public_acls       = false
-##  block_public_policy     = false
-##  ignore_public_acls      = false
-##  restrict_public_buckets = false
-##}
-##
-###ACL
-###resource "aws_s3_bucket_acl" "my-bucket" {
-## # bucket = aws_s3_bucket.New_Customer_Bucket.id
-##  #acl    = "private"
-###}
+# No change required in this file
+
+resource "aws_s3_bucket" "New_Customer_Bucket" {
+  bucket = "${var.CUSTOMER_NAME}.vultara.com"
+  region = var.region
+  force_destroy = true
+}
+
+#Public Access
+resource "aws_s3_bucket_public_access_block" "New_Customer_Bucket_Permissions_Block" {
+  bucket = aws_s3_bucket.New_Customer_Bucket.bucket
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+#ACL
+#resource "aws_s3_bucket_acl" "my-bucket" {
+ # bucket = aws_s3_bucket.New_Customer_Bucket.id
+  #acl    = "private"
+#}
+
 ###########################################s3 report bucket#######################################
 ##
 ##
