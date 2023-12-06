67a23fd8ed5780808fb86f3be6dba60ec772221e
mohamed-mansour8 <145043670+mohamed-mansour8@users.noreply.github.com>
2023-12-06 17:14:17 +0200

    Update s3_creation.tf
    Signed-off-by: mohamed-mansour8 <145043670+mohamed-mansour8@users.noreply.github.com>

--- primary_services_code/s3_creation.tf
+++ primary_services_code/s3_creation.tf
@@ -1,137 +1,137 @@
-# No change required in this file
-
-resource "aws_s3_bucket" "New_Customer_Bucket" {
-  bucket = "${var.CUSTOMER_NAME}.vultara.com"
-  force_destroy = true
-}
-
-#Public Access
-resource "aws_s3_bucket_public_access_block" "New_Customer_Bucket_Permissions_Block" {
-  bucket = aws_s3_bucket.New_Customer_Bucket.bucket
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
-
-#ACL
-#resource "aws_s3_bucket_acl" "my-bucket" {
- # bucket = aws_s3_bucket.New_Customer_Bucket.id
-  #acl    = "private"
-#}
-#########################################s3 report bucket#######################################
-
-
-resource "aws_s3_bucket" "reports-bucket" {
-  bucket = "${var.CUSTOMER_NAME}-reports-bucket"
-  force_destroy = true
-}
-
-
-
-resource "aws_s3_bucket_ownership_controls" "ownership-reports-bucket" {
-  bucket = aws_s3_bucket.reports-bucket.id
-  rule {
-    object_ownership = "ObjectWriter"
-  }
-}
-
-resource "aws_s3_bucket_acl" "acl-reports-bucket" {
-  depends_on = [aws_s3_bucket_ownership_controls.ownership-reports-bucket]
-
-  bucket = aws_s3_bucket.reports-bucket.id
-  acl    = "private"
-}
-
-
-resource "aws_s3_bucket_lifecycle_configuration" "lifecycle-reports-bucket" {
-   
-  bucket = aws_s3_bucket.reports-bucket.id
-  rule {
-    id      = "Expire current versions of objects After 7 Days"
-    status  = "Enabled"
-
-
-
-    expiration {
-      days = 7
-    }
-  }
-}
-
-
-
-#######################################################CVE&CPEBucket######################################################
-
-
-#resource "aws_s3_bucket" "cve-cpe-bucket" {
-#  bucket = "${var.CUSTOMER_NAME}-cve-cpe-bucket"
-#  force_destroy = true
-#}
-#
-#
-#
-#resource "aws_s3_bucket_ownership_controls" "ownership-cve-cpe-bucket" {
-#  bucket = aws_s3_bucket.cve-cpe-bucket.id
-#  rule {
-#    object_ownership = "ObjectWriter"
-#  }
-#}
-#
-#resource "aws_s3_bucket_acl" "acl-cve-cpe-bucket" {
-#  depends_on = [aws_s3_bucket_ownership_controls.ownership-cve-cpe-bucket]
-#
-#  bucket = aws_s3_bucket.cve-cpe-bucket.id
-#  acl    = "private"
-#}
-#
-#
-#resource "aws_s3_bucket_lifecycle_configuration" "lifecycle-cve-cpe-bucket" {
-#   
-#  bucket = aws_s3_bucket.cve-cpe-bucket.id
-#  rule {
-#    id      = "Expire current versions of objects After 7 Days"
-#    status  = "Enabled"
-#
-#
-#
-#    expiration {
-#      days = 7
-#    }
-#  }
-#}
-
-###############################################help-doc####################################################
-
-resource "aws_s3_bucket" "help-doc-bucket" {
-  bucket = "${var.CUSTOMER_NAME}-help-doc-bucket"
-  force_destroy = true
-}
-
-
-#Public Access
-resource "aws_s3_bucket_public_access_block" "Permissions_Block" {
-  bucket = aws_s3_bucket.help-doc-bucket.bucket
-}
-
-
-resource "aws_s3_bucket_public_access_block" "help-doc-bucket-Permissions_Block" {
-  bucket = aws_s3_bucket.help-doc-bucket.bucket
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
-
-######################################Production File Storage######################################
-resource "aws_s3_bucket" "production-file-bucket" {
-  bucket = "${var.CUSTOMER_NAME}-production-file-bucket"
-  force_destroy = true
-}
-#####################################import bucket##################################################
-resource "aws_s3_bucket" "import-bucket" {
-  bucket = "${var.CUSTOMER_NAME}-import-bucket"
-  force_destroy = true
-}
+### No change required in this file
+##
+##resource "aws_s3_bucket" "New_Customer_Bucket" {
+##  bucket = "${var.CUSTOMER_NAME}.vultara.com"
+##  force_destroy = true
+##}
+##
+###Public Access
+##resource "aws_s3_bucket_public_access_block" "New_Customer_Bucket_Permissions_Block" {
+##  bucket = aws_s3_bucket.New_Customer_Bucket.bucket
+##
+##  block_public_acls       = false
+##  block_public_policy     = false
+##  ignore_public_acls      = false
+##  restrict_public_buckets = false
+##}
+##
+###ACL
+###resource "aws_s3_bucket_acl" "my-bucket" {
+## # bucket = aws_s3_bucket.New_Customer_Bucket.id
+##  #acl    = "private"
+###}
+###########################################s3 report bucket#######################################
+##
+##
+##resource "aws_s3_bucket" "reports-bucket" {
+##  bucket = "${var.CUSTOMER_NAME}-reports-bucket"
+##  force_destroy = true
+##}
+##
+##
+##
+##resource "aws_s3_bucket_ownership_controls" "ownership-reports-bucket" {
+##  bucket = aws_s3_bucket.reports-bucket.id
+##  rule {
+##    object_ownership = "ObjectWriter"
+##  }
+##}
+##
+##resource "aws_s3_bucket_acl" "acl-reports-bucket" {
+##  depends_on = [aws_s3_bucket_ownership_controls.ownership-reports-bucket]
+##
+##  bucket = aws_s3_bucket.reports-bucket.id
+##  acl    = "private"
+##}
+##
+##
+##resource "aws_s3_bucket_lifecycle_configuration" "lifecycle-reports-bucket" {
+##   
+##  bucket = aws_s3_bucket.reports-bucket.id
+##  rule {
+##    id      = "Expire current versions of objects After 7 Days"
+##    status  = "Enabled"
+##
+##
+##
+##    expiration {
+##      days = 7
+##    }
+##  }
+##}
+##
+##
+##
+#########################################################CVE&CPEBucket######################################################
+##
+##
+###resource "aws_s3_bucket" "cve-cpe-bucket" {
+###  bucket = "${var.CUSTOMER_NAME}-cve-cpe-bucket"
+###  force_destroy = true
+###}
+###
+###
+###
+###resource "aws_s3_bucket_ownership_controls" "ownership-cve-cpe-bucket" {
+###  bucket = aws_s3_bucket.cve-cpe-bucket.id
+###  rule {
+###    object_ownership = "ObjectWriter"
+###  }
+###}
+###
+###resource "aws_s3_bucket_acl" "acl-cve-cpe-bucket" {
+###  depends_on = [aws_s3_bucket_ownership_controls.ownership-cve-cpe-bucket]
+###
+###  bucket = aws_s3_bucket.cve-cpe-bucket.id
+###  acl    = "private"
+###}
+###
+###
+###resource "aws_s3_bucket_lifecycle_configuration" "lifecycle-cve-cpe-bucket" {
+###   
+###  bucket = aws_s3_bucket.cve-cpe-bucket.id
+###  rule {
+###    id      = "Expire current versions of objects After 7 Days"
+###    status  = "Enabled"
+###
+###
+###
+###    expiration {
+###      days = 7
+###    }
+###  }
+###}
+##
+#################################################help-doc####################################################
+##
+##resource "aws_s3_bucket" "help-doc-bucket" {
+##  bucket = "${var.CUSTOMER_NAME}-help-doc-bucket"
+##  force_destroy = true
+##}
+##
+##
+###Public Access
+##resource "aws_s3_bucket_public_access_block" "Permissions_Block" {
+##  bucket = aws_s3_bucket.help-doc-bucket.bucket
+##}
+##
+##
+##resource "aws_s3_bucket_public_access_block" "help-doc-bucket-Permissions_Block" {
+##  bucket = aws_s3_bucket.help-doc-bucket.bucket
+##
+##  block_public_acls       = false
+##  block_public_policy     = false
+##  ignore_public_acls      = false
+##  restrict_public_buckets = false
+##}
+##
+########################################Production File Storage######################################
+##resource "aws_s3_bucket" "production-file-bucket" {
+##  bucket = "${var.CUSTOMER_NAME}-production-file-bucket"
+##  force_destroy = true
+##}
+#######################################import bucket##################################################
+##resource "aws_s3_bucket" "import-bucket" {
+##  bucket = "${var.CUSTOMER_NAME}-import-bucket"
+##  force_destroy = true
+##}
