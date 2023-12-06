e12b3158def9039c296e4a49a90b96c18f3e85fe
mohamed-mansour8 <145043670+mohamed-mansour8@users.noreply.github.com>
2023-12-06 10:43:25 +0200

    Update s3-endpoint.tf
    Signed-off-by: mohamed-mansour8 <145043670+mohamed-mansour8@users.noreply.github.com>

--- primary_services_code/s3-endpoint.tf
+++ primary_services_code/s3-endpoint.tf
@@ -38,8 +38,9 @@ resource "aws_vpc_endpoint_policy" "s3-private-policy" {
       "Effect": "Allow",
       "Resource": [ "arn:aws:s3:::${var.CUSTOMER_NAME}.vultara.com/*","arn:aws:s3:::${var.CUSTOMER_NAME}.vultara.com",#changed to public when making the new archteture
                     "arn:aws:s3:::${var.CUSTOMER_NAME}-reports-bucket/*","arn:aws:s3:::${var.CUSTOMER_NAME}-reports-bucket",
-                    "arn:aws:s3:::${var.CUSTOMER_NAME}-cve-cpe-bucket/*","arn:aws:s3:::${var.CUSTOMER_NAME}-cve-cpe-bucket",
+                    #"arn:aws:s3:::${var.CUSTOMER_NAME}-cve-cpe-bucket/*","arn:aws:s3:::${var.CUSTOMER_NAME}-cve-cpe-bucket",
                     "arn:aws:s3:::${var.CUSTOMER_NAME}-help-doc-bucket/*","arn:aws:s3:::${var.CUSTOMER_NAME}-help-doc-bucket" ,
+                    "arn:aws:s3:::${var.CUSTOMER_NAME}-import-bucket/*","arn:aws:s3:::${var.CUSTOMER_NAME}-import-bucket" ,
                      "arn:aws:s3:::docker-images-prod/*","arn:aws:s3:::docker-images-prod",
                      "arn:aws:s3:::prod-us-east-1-starport-layer-bucket/*","arn:aws:s3:::prod-us-east-1-starport-layer-bucket"],
       "Principal": "*"
