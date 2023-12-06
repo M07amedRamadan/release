0220e0d246f455f3f680b06df801463452792742
mohamed-mansour8 <145043670+mohamed-mansour8@users.noreply.github.com>
2023-12-06 18:42:23 +0200

    Update bucket_policy.tf
    Signed-off-by: mohamed-mansour8 <145043670+mohamed-mansour8@users.noreply.github.com>

--- primary_services_code/bucket_policy.tf
+++ primary_services_code/bucket_policy.tf
@@ -1,18 +1,18 @@
-### No change required in this file
-##
-##resource "aws_s3_bucket_policy" "hosting_bucket_policy" {
-##  bucket = aws_s3_bucket.New_Customer_Bucket.id
-##
-##  policy = jsonencode({
-##    "Version" : "2012-10-17",
-##    "Statement" : [
-##      {
-##        "Effect" : "Allow",
-##        "Principal" : "*",
-##        "Action" : "s3:GetObject",
-##        "Resource" : "arn:aws:s3:::${var.CUSTOMER_NAME}.vultara.com/*"
-##      }
-##    ]
-##  })
-##  depends_on = [aws_s3_bucket_public_access_block.Permissions_Block]
-##}
+# No change required in this file
+
+resource "aws_s3_bucket_policy" "hosting_bucket_policy" {
+  bucket = aws_s3_bucket.New_Customer_Bucket.id
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : "*",
+        "Action" : "s3:GetObject",
+        "Resource" : "arn:aws:s3:::${var.CUSTOMER_NAME}.vultara.com/*"
+      }
+    ]
+  })
+  depends_on = [aws_s3_bucket_public_access_block.Permissions_Block]
+}
