b1ffb9a87806fa5f8836bd1ab747c24d98c20dca
mohamed-mansour8 <145043670+mohamed-mansour8@users.noreply.github.com>
2023-12-11 01:08:08 +0200

    Create s3Cors.tf
    Signed-off-by: mohamed-mansour8 <145043670+mohamed-mansour8@users.noreply.github.com>

---
+++ primary_services_code/s3Cors.tf
@@ -0,0 +1,10 @@
+resource "aws_s3_bucket_cors_configuration" "s3-cors" {
+  bucket = aws_s3_bucket.import-bucket.id
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["PUT", "POST", "DELETE", "GET"]
+    allowed_origins = ["https://nikolamotor.vultara.com", "http://localhost:4200"]
+    expose_headers  = []
+  }
+}
