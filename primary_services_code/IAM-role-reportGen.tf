303a2d386c846adc592b10cf230f04f6b7c34c84
mohamed-mansour8 <145043670+mohamed-mansour8@users.noreply.github.com>
2023-12-13 11:53:25 +0200

    Create IAM-role-reportGen.tf
    Signed-off-by: mohamed-mansour8 <145043670+mohamed-mansour8@users.noreply.github.com>

---
+++ primary_services_code/IAM-role-reportGen.tf
@@ -0,0 +1,64 @@
+resource "aws_iam_role" "report_role" {
+  name = "${var.CUSTOMER_NAME}_report_role"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy  = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "s3:*",
+            "Resource": [
+                "arn:aws:s3:::${var.CUSTOMER_NAME}-reports-bucket",
+                "arn:aws:s3:::${var.CUSTOMER_NAME}-reports-bucket/*"
+            ]
+        }
+    ]
+})
+
+  policy  = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "s3:*",
+            "Resource": [
+                "arn:aws:s3:::${var.CUSTOMER_NAME}-reports-bucket",
+                "arn:aws:s3:::${var.CUSTOMER_NAME}-reports-bucket/*"
+            ]
+        }
+    ]
+})
+
+}
+
+resource "aws_iam_role_policy_attachment" "ContainerRegistry" {
+  role       = aws_iam_role.report_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonSSMManaged" {
+  role       = aws_iam_role.report_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_role_policy_attachment" "send_email_only" {
+  role       = aws_iam_role.report_role.name
+  policy_arn = "arn:aws:iam::837491041518:policy/send_email_only_policy"
+}
+
+resource "aws_iam_role_policy_attachment" "secret-mgr-read" {
+  role       = aws_iam_role.report_role.name
+  policy_arn = "arn:aws:iam::837491041518:policy/secret-mgr-read-all-secret-policy"
+}
+
+resource "aws_iam_role_policy_attachment" "mgr-assume-role" {
+  role       = aws_iam_role.report_role.name
+  policy_arn = "arn:aws:iam::837491041518:policy/secret-mgr-assume-role-policy"
+}
+
+resource "aws_iam_role_policy_attachment" "cloud-watch-policy" {
+  role       = aws_iam_role.report_role.name
+  policy_arn = "arn:aws:iam::837491041518:policy/cloud-watch-policy"
+}
