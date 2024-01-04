24cc3da70d884285710676172149a2e771859638
MohamedRamadan2024 <155387871+MohamedRamadan2024@users.noreply.github.com>
2024-01-04 18:28:55 +0200

    Update IAM-role-reportGen.tf
    Signed-off-by: MohamedRamadan2024 <155387871+MohamedRamadan2024@users.noreply.github.com>

--- primary_services_code/IAM-role-reportGen.tf
+++ primary_services_code/IAM-role-reportGen.tf
@@ -3,7 +3,7 @@ resource "aws_iam_role" "report_role" {
 
   # Terraform's "jsonencode" function converts a
   # Terraform expression result to valid JSON syntax.
-  policy  = jsonencode({
+  assume_role_policy  = jsonencode({
     "Version": "2012-10-17",
     "Statement": [
         {
