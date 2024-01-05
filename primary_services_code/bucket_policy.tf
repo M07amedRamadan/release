f08b99f94415742828671620475891525a439b4d
MohamedRamadan2024 <155387871+MohamedRamadan2024@users.noreply.github.com>
2024-01-05 03:03:02 +0200

    Update bucket_policy.tf
    Signed-off-by: MohamedRamadan2024 <155387871+MohamedRamadan2024@users.noreply.github.com>

--- primary_services_code/bucket_policy.tf
+++ primary_services_code/bucket_policy.tf
@@ -18,23 +18,4 @@ resource "aws_s3_bucket_policy" "hosting_bucket_policy" {
 }
 
 
-resource "aws_s3_bucket_policy" "import-bucket-police" {
-  bucket = aws_s3_bucket.import-bucket.id
 
-  policy = jsonencode({
-    "Version": "2012-10-17",
-    "Id": "S3PolicyId1",
-    "Statement": [
-        {
-            "Sid": "AllowPublicRead",
-            "Effect": "Allow",
-            "Principal": "*",
-            "Action": [
-                "s3:GetObject",
-                "s3:PutObject"
-            ],
-            "Resource": "arn:aws:s3:::${var.CUSTOMER_NAME}-import-bucket/*"
-        }
-    ]
-})
-}
