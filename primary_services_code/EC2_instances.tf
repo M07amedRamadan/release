fdc54ca59427c1eaca99db1242cb2abc4663402c
MohamedRamadan2024 <155387871+MohamedRamadan2024@users.noreply.github.com>
2024-01-05 03:53:37 +0200

    Update EC2_instances.tf
    Signed-off-by: MohamedRamadan2024 <155387871+MohamedRamadan2024@users.noreply.github.com>

--- primary_services_code/EC2_instances.tf
+++ primary_services_code/EC2_instances.tf
@@ -1,16 +1,15 @@
-resource "aws_instance" "report_generator" {
-  ami           = var.ami
-  instance_type = var.instance_type
+resource "aws_instance" "report-generator" {
+  ami                    = var.ami  
+  instance_type          =  var.instance_type
   key_name               = "vultara-report-server-KP"
   vpc_security_group_ids = [aws_security_group.reportGenerator_SG.id]
   subnet_id              = aws_subnet.private_1.id
-  iam_instance_profile = aws_iam_role.report_role.name
-  tags = {
+  iam_instance_profile   = aws_iam_role.report_role.name
+tags = {
     Name = "${var.CUSTOMER_NAME}-report-Prod"
   }
   user_data = file("${path.module}/script.sh")
 }
-
 
 resource "aws_instance" "vultara_scheduler" {
   ami           = var.ami
