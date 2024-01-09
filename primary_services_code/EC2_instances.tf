b65a55d5154fad0deff9628b9091fa28fddfd42b
MohamedRamadan2024 <155387871+MohamedRamadan2024@users.noreply.github.com>
2024-01-09 19:50:36 +0200

    Update EC2_instances.tf
    Signed-off-by: MohamedRamadan2024 <155387871+MohamedRamadan2024@users.noreply.github.com>

--- primary_services_code/EC2_instances.tf
+++ primary_services_code/EC2_instances.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "report_generator" {
   ami                    = var.ami  
   instance_type          =  var.instance_type
-  key_name            = "vultara-trial-scheduler-KP"
+ # key_name            = "vultara-trial-scheduler-KP"
   #key_name              = "${var.key_name}"
   vpc_security_group_ids = [aws_security_group.reportGenerator_SG.id]
   subnet_id              = aws_subnet.private_1.id
@@ -16,7 +16,7 @@ tags = {
 resource "aws_instance" "vultara_scheduler" {
   ami           = var.ami
   instance_type = var.instance_type
-  key_name            = "vultara-trial-scheduler-KP"
+ # key_name            = "vultara-trial-scheduler-KP"
   #key_name              = "${var.key_name}"
   vpc_security_group_ids = [aws_security_group.schedulerServer_SG.id]
   subnet_id              = aws_subnet.private_2.id
