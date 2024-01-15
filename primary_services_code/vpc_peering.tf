96a59070dc41d15bc8e9f3c7dab36041670b0323
MohamedRamadan2024 <155387871+MohamedRamadan2024@users.noreply.github.com>
2024-01-15 18:17:05 +0200

    Update vpc_peering.tf
    Signed-off-by: MohamedRamadan2024 <155387871+MohamedRamadan2024@users.noreply.github.com>

--- primary_services_code/vpc_peering.tf
+++ primary_services_code/vpc_peering.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc_peering_connection" "peering_connection" {
   vpc_id      = "vpc-01ff5914b2252b003"              # ID of the existing VPC in us-east-1
   peer_vpc_id = aws_vpc.New_Customer_VPC.id          # ID of the new VPC in another region
-  peer_region = "us-west-1"                          # Region of the peer VPC
+  #peer_region = "us-west-1"                          # Region of the peer VPC
   auto_accept = true
 
 tags = {
