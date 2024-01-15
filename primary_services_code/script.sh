a080ab74853a27c7a1e98566a42bb5fef7fecaab
MohamedRamadan2024 <155387871+MohamedRamadan2024@users.noreply.github.com>
2024-01-15 16:24:55 +0200

    Update script.sh
    Signed-off-by: MohamedRamadan2024 <155387871+MohamedRamadan2024@users.noreply.github.com>

--- primary_services_code/script.sh
+++ primary_services_code/script.sh
@@ -6,3 +6,12 @@ usermod -aG docker ubuntu
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
+#####integrate Cloud Watch with instances.#####
+# Install the CloudWatch agent
+curl -O https://s3.amazonaws.com/amazoncloudwatch-agent/linux/amd64/latest/AmazonCloudWatchAgent.zip
+unzip AmazonCloudWatchAgent.zip
+sudo ./install.sh
+# Configure the CloudWatch agent with default settings
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-config-wizard -n -s
+# Start the CloudWatch agent
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json -s
