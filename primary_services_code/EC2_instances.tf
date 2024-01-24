22912ca2ff743a1b03f1a89c5d1061812e399833
MohamedRamadan2024 <155387871+MohamedRamadan2024@users.noreply.github.com>
2024-01-25 01:46:22 +0200

    Update EC2_instances.tf
    Signed-off-by: MohamedRamadan2024 <155387871+MohamedRamadan2024@users.noreply.github.com>

--- primary_services_code/EC2_instances.tf
+++ primary_services_code/EC2_instances.tf
@@ -1,13 +1,13 @@
 resource "aws_instance" "report_generator" {
   ami                    = var.ami  
   instance_type          =  var.instance_type
- # key_name               = "vultara-report-server-KP"
+  key_name               = "vultara-report-server-KP"
   vpc_security_group_ids = [aws_security_group.reportGenerator_SG.id]
   subnet_id              = aws_subnet.private_1.id
   iam_instance_profile   = aws_iam_instance_profile.instance_profile.name
 
 tags = {
-    Name = "${var.CUSTOMER_NAME}-report-Prod"
+  Name = "${var.CUSTOMER_NAME}-report-Prod"
   }
   user_data = file("${path.module}/script.sh")
 }
@@ -15,10 +15,11 @@ tags = {
 resource "aws_instance" "vultara_scheduler" {
   ami           = var.ami
   instance_type = var.instance_type
- # key_name               = "vultara-trial-scheduler-KP"
+  key_name               = "vultara-trial-scheduler-KP"
   vpc_security_group_ids = [aws_security_group.schedulerServer_SG.id]
   subnet_id              = aws_subnet.private_2.id
-  iam_instance_profile   = "scheduler-server-role"
+  iam_instance_profile   = aws_iam_instance_profile.instance_profile.name
+
   tags = {
   Name = "${var.CUSTOMER_NAME}-scheduler-Prod"
   }
