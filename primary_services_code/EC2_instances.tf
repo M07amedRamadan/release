1b2519083b459b1aee993fd202445f7b83b339c9
MohamedRamadan2024 <155387871+MohamedRamadan2024@users.noreply.github.com>
2024-01-29 21:11:43 +0200

    Update EC2_instances.tf
    Signed-off-by: MohamedRamadan2024 <155387871+MohamedRamadan2024@users.noreply.github.com>

--- primary_services_code/EC2_instances.tf
+++ primary_services_code/EC2_instances.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   alias  = "key"
   region = "us-east-1"
-  access_key = "secrets.AWS_ACCESS_KEY_ID"
-  secret_key = "secrets.AWS_SECRET_ACCESS_KEY"
+  access_key = secrets.AWS_ACCESS_KEY_ID
+  secret_key = secrets.AWS_SECRET_ACCESS_KEY
   # Accepter's credentials if in another account.
 }
 
