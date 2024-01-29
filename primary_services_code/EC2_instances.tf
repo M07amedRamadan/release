12675964efbd50551ed26a7780fee53a8138bf39
MohamedRamadan2024 <155387871+MohamedRamadan2024@users.noreply.github.com>
2024-01-29 23:43:42 +0200

    Update EC2_instances.tf
    Signed-off-by: MohamedRamadan2024 <155387871+MohamedRamadan2024@users.noreply.github.com>

--- primary_services_code/EC2_instances.tf
+++ primary_services_code/EC2_instances.tf
@@ -21,7 +21,7 @@ tags = {
 resource "aws_instance" "vultara_scheduler" {
   ami                    = var.ami
   instance_type          = var.instance_type
- # key_name               = data.aws_key_pair.scheduler_key.key_name
+ #key_name               = data.aws_key_pair.scheduler_key.key_name
   vpc_security_group_ids = [aws_security_group.schedulerServer_SG.id]
   subnet_id              = aws_subnet.private_2.id
   iam_instance_profile   = aws_iam_instance_profile.instance_profile.name
@@ -42,11 +42,6 @@ data "aws_key_pair" "scheduler_key" {
 data "aws_key_pair" "report_key" {
   provider = aws.key
   key_name = "vultara-report-server-KP"
-}
-# Create a new key pair in the destination region using the public key from the source region
-resource "aws_key_pair" "report_key" {
-  key_name   = "new_key_name"
-  public_key = data.aws_key_pair.report_key.public_key
 }
 
 output "report_key" {
