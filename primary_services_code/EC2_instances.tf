935d4a8cecea6e97eb56fe8abe72301b9b4c32a9
MohamedRamadan2024 <155387871+MohamedRamadan2024@users.noreply.github.com>
2024-01-29 20:56:24 +0200

    Update EC2_instances.tf
    Signed-off-by: MohamedRamadan2024 <155387871+MohamedRamadan2024@users.noreply.github.com>

--- primary_services_code/EC2_instances.tf
+++ primary_services_code/EC2_instances.tf
@@ -8,7 +8,7 @@ provider "aws" {
 resource "aws_instance" "report_generator" {
   ami                    = var.ami  
   instance_type          =  var.instance_type
-  key_name               = data.aws_key_pair.report_key.key_name
+  key_name               = aws_key_pair.report_key_new.key_name
   vpc_security_group_ids = [aws_security_group.reportGenerator_SG.id]
   subnet_id              = aws_subnet.private_1.id
   iam_instance_profile   = aws_iam_instance_profile.instance_profile.name
@@ -22,7 +22,7 @@ tags = {
 resource "aws_instance" "vultara_scheduler" {
   ami           = var.ami
   instance_type = var.instance_type
-  key_name               = data.aws_key_pair.scheduler_key.key_name
+  key_name               = aws_key_pair.scheduler_key_new.key_name
   vpc_security_group_ids = [aws_security_group.schedulerServer_SG.id]
   subnet_id              = aws_subnet.private_2.id
   iam_instance_profile   = aws_iam_instance_profile.instance_profile.name
@@ -44,6 +44,20 @@ data "aws_key_pair" "report_key" {
   provider = aws.key
   key_name = "vultara-report-server-KP"
 }
+# Create a new key pair in us-east-2 using the public key from us-east-1
+resource "aws_key_pair" "scheduler_key_new" {
+  key_name   = "vultara-trial-scheduler-KP"
+  public_key = data.aws_key_pair.scheduler_key.public_key
+  region     = var.region
+}
+# Create a new key pair in us-east-2 using the public key from us-east-1
+resource "aws_key_pair" "report_key_new" {
+  key_name   = "vultara-report-server-KP"
+  public_key = data.aws_key_pair.report_key.public_key
+  region     = var.region
+}
+
+
 output "report_key" {
 value = data.aws_key_pair.report_key.id
 }
