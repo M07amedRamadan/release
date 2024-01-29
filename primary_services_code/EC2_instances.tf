3cd463c835a398800f9524b977442bcb2fc8f919
MohamedRamadan2024 <155387871+MohamedRamadan2024@users.noreply.github.com>
2024-01-29 19:50:04 +0200

    Update EC2_instances.tf
    Signed-off-by: MohamedRamadan2024 <155387871+MohamedRamadan2024@users.noreply.github.com>

--- primary_services_code/EC2_instances.tf
+++ primary_services_code/EC2_instances.tf
@@ -1,3 +1,10 @@
+provider "aws" {
+  alias  = "key"
+  region = "us-east-1"
+
+  # Accepter's credentials if in another account.
+}
+
 resource "aws_instance" "report_generator" {
   ami                    = var.ami  
   instance_type          =  var.instance_type
@@ -31,11 +38,13 @@ resource "aws_instance" "vultara_scheduler" {
 
 # Obtain the public key from an existing key pair in us-east-1
 data "aws_key_pair" "scheduler_key" {
+  provider = aws.key
   key_name = "vultara-trial-scheduler-KP"
  #region   = "us-east-1"
 }
 # Obtain the public key from an existing key pair in us-east-1
 data "aws_key_pair" "report_key" {
+  provider = aws.key
   key_name = "vultara-report-server-KP"
   #region   = "us-east-1"
 }
