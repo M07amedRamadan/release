3e1e00a7298c0019d666c44b2620accb1ca6dbf3
MohamedRamadan2024 <155387871+MohamedRamadan2024@users.noreply.github.com>
2024-01-29 20:42:56 +0200

    Update EC2_instances.tf
    Signed-off-by: MohamedRamadan2024 <155387871+MohamedRamadan2024@users.noreply.github.com>

--- primary_services_code/EC2_instances.tf
+++ primary_services_code/EC2_instances.tf
@@ -8,7 +8,7 @@ provider "aws" {
 resource "aws_instance" "report_generator" {
   ami                    = var.ami  
   instance_type          =  var.instance_type
-  key_name               = "data.aws_key_pair.report_key.key_name"
+  key_name               = data.aws_key_pair.report_key.key_name
   vpc_security_group_ids = [aws_security_group.reportGenerator_SG.id]
   subnet_id              = aws_subnet.private_1.id
   iam_instance_profile   = aws_iam_instance_profile.instance_profile.name
@@ -22,7 +22,7 @@ tags = {
 resource "aws_instance" "vultara_scheduler" {
   ami           = var.ami
   instance_type = var.instance_type
-  key_name               = "data.aws_key_pair.scheduler_key.key_name"
+  key_name               = data.aws_key_pair.scheduler_key.key_name
   vpc_security_group_ids = [aws_security_group.schedulerServer_SG.id]
   subnet_id              = aws_subnet.private_2.id
   iam_instance_profile   = aws_iam_instance_profile.instance_profile.name
@@ -36,15 +36,15 @@ resource "aws_instance" "vultara_scheduler" {
 
 # Obtain the public key from an existing key pair in us-east-1
 data "aws_key_pair" "scheduler_key" {
-  provider = aws.key
+  #provider = aws.key
   key_name = "vultara-trial-scheduler-KP"
- #region   = "us-east-1"
+  region   = "us-east-1"
 }
 # Obtain the public key from an existing key pair in us-east-1
 data "aws_key_pair" "report_key" {
-  provider = aws.key
+  #provider = aws.key
   key_name = "vultara-report-server-KP"
-  #region   = "us-east-1"
+  region   = "us-east-1"
 }
 output "report_key" {
 value = data.aws_key_pair.report_key.id
