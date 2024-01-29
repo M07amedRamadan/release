e817ad12303129c560813878194bed62ed01f6da
MohamedRamadan2024 <155387871+MohamedRamadan2024@users.noreply.github.com>
2024-01-29 23:17:41 +0200

    Update vpc_peering.tf
    Signed-off-by: MohamedRamadan2024 <155387871+MohamedRamadan2024@users.noreply.github.com>

--- primary_services_code/vpc_peering.tf
+++ primary_services_code/vpc_peering.tf
@@ -4,13 +4,14 @@ provider "aws" {
 }
 
 resource "aws_vpc_peering_connection" "peering_connection" {
-  peer_region = "us-east-1"                          # Region of the peer VPC
+  peer_region = "us-east-1"                          # Region of the vultara VPC
+  peer_owner_id = "837491041518"
   vpc_id      = data.aws_vpc.vultara_vpc.id
-  #vpc_id      = "vpc-01ff5914b2252b003"             # ID of the vultara_vpc in us-east-1
   peer_vpc_id = aws_vpc.New_Customer_VPC.id          # ID of the new VPC in another region
 
 tags = {
     Name = "VPC-Peering-Vultara-${var.CUSTOMER_NAME}-vpc"
+    Side = "Requester"
   }
 }
 
