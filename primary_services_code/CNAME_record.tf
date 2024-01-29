bf49968d50fb1a0720df62764520d5f16d74be25
MohamedRamadan2024 <155387871+MohamedRamadan2024@users.noreply.github.com>
2024-01-30 01:12:06 +0200

    Update CNAME_record.tf
    Signed-off-by: MohamedRamadan2024 <155387871+MohamedRamadan2024@users.noreply.github.com>

--- primary_services_code/CNAME_record.tf
+++ primary_services_code/CNAME_record.tf
@@ -5,7 +5,11 @@ resource "aws_route53_record" "www-dev" {
   ttl     = 3600
   records = ["${var.cloudfront_id}"]
 }
-  
+
+#data "aws_route53_zone" "vultara_zone" {
+#  name = "example.com"  # Replace with the actual domain name
+#}
+
 output "hosted_zone" {
 value   =    aws_route53_zone.primary.zone_id 
 }
