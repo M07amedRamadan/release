f6907467983b2563b08ad4945dbc6107375cc77f
MohamedRamadan2024 <155387871+MohamedRamadan2024@users.noreply.github.com>
2024-01-31 16:14:51 +0200

    Update schedular_script.sh
    Signed-off-by: MohamedRamadan2024 <155387871+MohamedRamadan2024@users.noreply.github.com>

--- primary_services_code/schedular_script.sh
+++ primary_services_code/schedular_script.sh
@@ -6,6 +6,7 @@ usermod -aG docker ubuntu
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
+#Installing CloudWatch and its configuration.
 # Retrieve the instance ID and instance name using the AWS CLI
 instance_id=$(ec2metadata --instance-id)
 instance_name=$(aws ec2 describe-instances --instance-ids "$instance_id" --query 'Reservations[0].Instances[0].Tags[?Key==`Name`].Value' --output text)
@@ -50,3 +51,17 @@ EOF"
 sudo systemctl start amazon-cloudwatch-agent
 # Enable the CloudWatch Agent service to start on boot
 sudo systemctl enable amazon-cloudwatch-agent
+#Installing MongoDb and its configuration to automate populating new customer database with the new customer template.
+#Download the GnuPG and CURL utility
+sudo apt-get install -y gnupg curl
+#Import the MongoDB public GPG key to retrieve the installation package.
+sudo curl -fsSL https://pgp.mongodb.com/server-7.0.asc | sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg --dearmor
+#Create a list file for installation
+sudo echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+#Refresh the APT repository to synchronize the local package database.
+sudo apt-get update
+#Tnstall the latest stable version of MongoDB
+sudo apt-get install -y mongodb-org
+#Start and Enable the mongoDB
+sudo systemctl start mongod
+sudo systemctl enable mongod
