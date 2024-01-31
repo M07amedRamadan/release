eab1d8b522d8b1846c8880447a1a22e7dcb29d9d
MohamedRamadan2024 <155387871+MohamedRamadan2024@users.noreply.github.com>
2024-01-31 18:19:05 +0200

    Update key EC2_instances.tf
    Signed-off-by: MohamedRamadan2024 <155387871+MohamedRamadan2024@users.noreply.github.com>

--- primary_services_code/EC2_instances.tf
+++ primary_services_code/EC2_instances.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "report_generator" {
   ami                    = var.ami  
   instance_type          = var.instance_type
   #key_name              = "vultara-report-server-KP" #create this key in the NewCustomer region
-  key_name               = data.aws_key_pair.report_key.key_name  #for the key in a different region but not worked yet 
+  #key_name               = data.aws_key_pair.report_key.key_name  #for the key in a different region but not worked yet 
   vpc_security_group_ids = [aws_security_group.reportGenerator_SG.id]
   subnet_id              = aws_subnet.private_1.id
   iam_instance_profile   = aws_iam_instance_profile.instance_profile.name
@@ -23,7 +23,7 @@ resource "aws_instance" "vultara_scheduler" {
   ami                    = var.ami
   instance_type          = var.instance_type
   #key_name              = "vultara-trial-scheduler-KPP" #create this key in the NewCustomer region
-  key_name               = data.aws_key_pair.scheduler_key.key_name  #for the key in a different region but not worked yet 
+  #key_name               = data.aws_key_pair.scheduler_key.key_name  #for the key in a different region but not worked yet 
   vpc_security_group_ids = [aws_security_group.schedulerServer_SG.id]
   subnet_id              = aws_subnet.private_2.id
   iam_instance_profile   = aws_iam_instance_profile.instance_profile.name
