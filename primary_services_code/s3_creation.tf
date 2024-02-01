afdb54923c5c25477684c8bbaebe1ba07428711c
MohamedRamadan2024 <155387871+MohamedRamadan2024@users.noreply.github.com>
2024-02-01 19:44:15 +0200

    Update s3_creation.tf
    Signed-off-by: MohamedRamadan2024 <155387871+MohamedRamadan2024@users.noreply.github.com>

--- primary_services_code/s3_creation.tf
+++ primary_services_code/s3_creation.tf
@@ -63,7 +63,7 @@ resource "aws_s3_bucket" "production-file-bucket" {
   force_destroy = true
 }
 #####################################import bucket##################################################
-resource "aws_s3_bucket" "import_bucket" {
+resource "aws_s3_bucket" "import-bucket" {
   bucket = "${var.CUSTOMER_NAME}-import-bucket"
   force_destroy = true
 }
