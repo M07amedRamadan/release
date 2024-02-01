7526d9ad47310c811ab775b6c3dc8dd5bb459acc
MohamedRamadan2024 <155387871+MohamedRamadan2024@users.noreply.github.com>
2024-02-01 20:52:07 +0200

    Update s3_creation.tf
    Signed-off-by: MohamedRamadan2024 <155387871+MohamedRamadan2024@users.noreply.github.com>

--- primary_services_code/s3_creation.tf
+++ primary_services_code/s3_creation.tf
@@ -71,7 +71,7 @@ resource "aws_s3_bucket" "import-bucket" {
 resource "aws_s3_bucket_public_access_block" "Block-public-access" {
   bucket = aws_s3_bucket.import-bucket.id
 
-  block_public_acls       = true
+  block_public_acls       = false
   block_public_policy     = false
   ignore_public_acls      = true
   restrict_public_buckets = true
