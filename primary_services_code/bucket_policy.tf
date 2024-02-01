e4f742722123ab5984cb62ff5d61375ac9036645
MohamedRamadan2024 <155387871+MohamedRamadan2024@users.noreply.github.com>
2024-02-01 17:13:33 +0200

    Update bucket_policy.tf
    Signed-off-by: MohamedRamadan2024 <155387871+MohamedRamadan2024@users.noreply.github.com>

--- primary_services_code/bucket_policy.tf
+++ primary_services_code/bucket_policy.tf
@@ -17,25 +17,25 @@ resource "aws_s3_bucket_policy" "hosting_bucket_policy" {
   depends_on = [aws_s3_bucket_public_access_block.New_Customer_Bucket_Permissions_Block]
 }
 
-resource "aws_s3_bucket_policy" "import-bucket-police" {
-  bucket = aws_s3_bucket.import-bucket.id
+#resource "aws_s3_bucket_policy" "import-bucket-police" {
+ # bucket = aws_s3_bucket.import-bucket.id
 
-  policy = jsonencode({
-    "Version": "2012-10-17",
-    "Id": "S3PolicyId1",
-    "Statement": [
-        {
-            "Sid": "AllowPublicRead",
-            "Effect": "Allow",
-            "Principal": "*",
-            "Action": [
-                "s3:GetObject",
-                "s3:PutObject"
-            ],
-            "Resource": "arn:aws:s3:::${var.CUSTOMER_NAME}-import-bucket/*"
-        }
-    ]
-})
-  depends_on = [aws_s3_bucket_public_access_block.New_Customer_Bucket_Permissions_Block]
-}
+ # policy = jsonencode({
+ #   "Version": "2012-10-17",
+ #   "Id": "S3PolicyId1",
+ #   "Statement": [
+ #       {
+ #           "Sid": "AllowPublicRead",
+ #           "Effect": "Allow",
+ #           "Principal": "*",
+ #           "Action": [
+ #               "s3:GetObject",
+ #               "s3:PutObject"
+ #           ],
+ #           "Resource": "arn:aws:s3:::${var.CUSTOMER_NAME}-import-bucket/*"
+#        }
+#    ]
+#})
+#  depends_on = [aws_s3_bucket_public_access_block.New_Customer_Bucket_Permissions_Block]
+#}
 
