d16cb2af6b5f0ed6b9f50c839ea11fb40c1abcb3
MohamedRamadan2024 <155387871+MohamedRamadan2024@users.noreply.github.com>
2024-04-26 21:16:00 +0300

    Update bucket_policy.tf
    Signed-off-by: MohamedRamadan2024 <155387871+MohamedRamadan2024@users.noreply.github.com>

--- primary_services_code/bucket_policy.tf
+++ primary_services_code/bucket_policy.tf
@@ -4,16 +4,20 @@ resource "aws_s3_bucket_policy" "hosting_bucket_policy" {
   bucket = aws_s3_bucket.New_Customer_Bucket.id
 
   policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
-      {
-        "Effect" : "Allow",
-        "Principal" : "*",
-        "Action" : "s3:GetObject",
-        "Resource" : "arn:aws:s3:::${var.CUSTOMER_NAME}.vultara.com/*"
-      }
-    ]
-  })
+     "Version": "2008-10-17",
+      "Id": "PolicyForCloudFrontPrivateContent",
+      "Statement": [
+        {
+          "Sid": "1",
+          "Effect": "Allow",
+          "Principal": {
+            "AWS": "arn:aws:iam::cloudfront:user/CloudFront Origin Access Identity ${aws_cloudfront_origin_access_identity.oai.iam_arn}"
+          },
+          "Action": "s3:GetObject",
+          "Resource": "arn:aws:s3:::${var.CUSTOMER_NAME}.vultara.com/*"
+        }
+      ]
+    })
   depends_on = [aws_s3_bucket_public_access_block.New_Customer_Bucket_Permissions_Block]
 }
 
