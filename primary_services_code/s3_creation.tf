b4bc4c40bfeed05508d8231e446e697ca0a0ddd0
MohamedRamadan2024 <155387871+MohamedRamadan2024@users.noreply.github.com>
2024-04-26 22:19:26 +0300

    Update s3_creation.tf
    Signed-off-by: MohamedRamadan2024 <155387871+MohamedRamadan2024@users.noreply.github.com>

--- primary_services_code/s3_creation.tf
+++ primary_services_code/s3_creation.tf
@@ -9,8 +9,8 @@ resource "aws_s3_bucket" "New_Customer_Bucket" {
 resource "aws_s3_bucket_public_access_block" "New_Customer_Bucket_Permissions_Block" {
   bucket = aws_s3_bucket.New_Customer_Bucket.bucket
 
-#  block_public_acls       = false
-  #block_public_policy     = false
+#  block_public_acls       = true
+  #block_public_policy     = true
   #ignore_public_acls      = true
   #restrict_public_buckets = true
 }
