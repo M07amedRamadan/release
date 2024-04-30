c8862e4de3c9a8010efc201cd0a551db9a2f68bb
MohamedRamadan2024 <155387871+MohamedRamadan2024@users.noreply.github.com>
2024-04-30 04:12:50 +0300

    Update IAM-Role-Instance.tf
    Signed-off-by: MohamedRamadan2024 <155387871+MohamedRamadan2024@users.noreply.github.com>

--- primary_services_code/IAM-Role-Instance.tf
+++ primary_services_code/IAM-Role-Instance.tf
@@ -4,7 +4,7 @@ data "aws_iam_role" "vultara_role" {
  
 #IAM Instance Profile Used to attach IAM role with ec2 
 resource "aws_iam_instance_profile" "instance_profile" {
-  name = "Ec2-instance-profile"
+  name = "${var.CUSTOMER_NAME}-instance-profile"
   role = data.aws_iam_role.vultara_role.name
 }
 
