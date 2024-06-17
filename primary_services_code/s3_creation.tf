b8b868a951b6e284201eb0ea5f25cbcd3cffcb83
MohamedRamadan2024 <155387871+MohamedRamadan2024@users.noreply.github.com>
2024-06-17 21:13:15 +0300

    Update s3_creation.tf
    Signed-off-by: MohamedRamadan2024 <155387871+MohamedRamadan2024@users.noreply.github.com>

--- primary_services_code/s3_creation.tf
+++ primary_services_code/s3_creation.tf
@@ -17,13 +17,10 @@ resource "aws_s3_bucket_public_access_block" "New_Customer_Bucket_Permissions_Bl
 
 #########################################s3 report bucket#######################################
 
-
 resource "aws_s3_bucket" "reports-bucket" {
   bucket = "${var.CUSTOMER_NAME}-reports-bucket"
   force_destroy = true
 }
-
-
 
 resource "aws_s3_bucket_ownership_controls" "ownership-reports-bucket" {
   bucket = aws_s3_bucket.reports-bucket.id
@@ -39,7 +36,6 @@ resource "aws_s3_bucket_acl" "acl-reports-bucket" {
   acl    = "private"
 }
 
-
 resource "aws_s3_bucket_lifecycle_configuration" "lifecycle-reports-bucket" {
    
   bucket = aws_s3_bucket.reports-bucket.id
@@ -47,28 +43,19 @@ resource "aws_s3_bucket_lifecycle_configuration" "lifecycle-reports-bucket" {
     id      = "Expire current versions of objects After 7 Days"
     status  = "Enabled"
 
-
-
     expiration {
       days = 7
     }
   }
 }
 
-
-
-######################################Production File Storage######################################
-resource "aws_s3_bucket" "production-file-bucket" {
-  bucket = "${var.CUSTOMER_NAME}-production-file-bucket"
-  force_destroy = true
-}
 #####################################import bucket##################################################
 resource "aws_s3_bucket" "import-bucket" {
   bucket = "${var.CUSTOMER_NAME}-import-bucket"
   force_destroy = true
 } 
 
-#still the import bucket private but only can access throw the policy only.
+#still, the import bucket is private but can be accessed through the policy only.
 resource "aws_s3_bucket_public_access_block" "Block-public-access" {
   bucket = aws_s3_bucket.import-bucket.id
 
