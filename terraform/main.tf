3cb0308198084d96f75a2d5040bdc38b47b0c7ad
MohamedRamadan2024 <mohamed.ramadan.bakry@vultara.com>
2024-12-17 20:13:47 +0200

    create secret

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,7 +19,8 @@ locals {
     local.existing_secret,
   # Update specific values in the secret
   {
-    "CVE_API_KEY" = contains(keys(local.existing_secret), "CVE_API_KEY"),
+    "CVE_API_KEY" = contains(keys(local.existing_secret), "${local.existing_secret["CVE_API_KEY"]}"),
+    "ATLASDB_ALGOREAD" = contains(keys(local.existing_secret), "${local.existing_secret["ATLASDB_ALGOREAD"]}"),
     "ACCESS_TOKEN" = contains(keys(local.existing_secret), "${random_password.access_token.result}") ? "${random_password.access_token.result}" : "${random_password.access_token.result}",
     "JWT_SECRET_KEY" = contains(keys(local.existing_secret), "${random_password.JWT_SECRET_KEY.result}") ? "${random_password.JWT_SECRET_KEY.result}" : "${random_password.JWT_SECRET_KEY.result}",
     "JWT_ACCESS_TOKEN_SECRET" = contains(keys(local.existing_secret), "${random_password.JWT_ACCESS_REFRESH_TOKEN_SECRET.result}") ? "${random_password.JWT_ACCESS_REFRESH_TOKEN_SECRET.result}" : "${random_password.JWT_ACCESS_REFRESH_TOKEN_SECRET.result}",
