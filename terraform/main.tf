4eb4dad90f8c86357d9554193f2f45d4c73f24b9
MohamedRamadan2024 <mohamed.ramadan.bakry@vultara.com>
2024-12-17 20:09:43 +0200

    creating new one with only desired strings

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,7 +19,7 @@ locals {
     local.existing_secret,
   # Update specific values in the secret
   {
-    "CVE_API_KEY" = contains(keys(local.existing_secret), "CVE_API_KEY"),
+    # "CVE_API_KEY" = contains(keys(local.existing_secret), "CVE_API_KEY"),
     "ACCESS_TOKEN" = contains(keys(local.existing_secret), "${random_password.access_token.result}") ? "${random_password.access_token.result}" : "${random_password.access_token.result}",
     "JWT_SECRET_KEY" = contains(keys(local.existing_secret), "${random_password.JWT_SECRET_KEY.result}") ? "${random_password.JWT_SECRET_KEY.result}" : "${random_password.JWT_SECRET_KEY.result}",
     "JWT_ACCESS_TOKEN_SECRET" = contains(keys(local.existing_secret), "${random_password.JWT_ACCESS_REFRESH_TOKEN_SECRET.result}") ? "${random_password.JWT_ACCESS_REFRESH_TOKEN_SECRET.result}" : "${random_password.JWT_ACCESS_REFRESH_TOKEN_SECRET.result}",
