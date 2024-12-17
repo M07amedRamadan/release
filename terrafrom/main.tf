3f825453769461981de15a40b9cf02e5e8b15984
MohamedRamadan2024 <mohamed.ramadan.bakry@vultara.com>
2024-12-17 13:44:43 +0200

    create AWS Secret Manager

--- terrafrom/main.tf
+++ terrafrom/main.tf
@@ -4,47 +4,69 @@ resource "aws_secretsmanager_secret" "secret" {
 }
 
 data "aws_secretsmanager_secret_version" "old_secret" {
-  secret_id = "<old-secret-id>"
-}
-resource "aws_secretsmanager_secret" "new_secret" {
-  name = "<new-secret-name>"
+  secret_id = "trial-application-user"
 }
 
 resource "aws_secretsmanager_secret_version" "new_secret_version" {
-  secret_id     = aws_secretsmanager_secret.new_secret.id
-  secret_string = data.aws_secretsmanager_secret_version.old_secret.secret_string
+  secret_id     = aws_secretsmanager_secret.secret.id
+  secret_string = jsonencode(local.updated_secret)
 }
 
+# Decode the existing secret JSON
+locals {
+  existing_secret = jsondecode(data.aws_secretsmanager_secret_version.old_secret.secret_string)
 
-(* resource "aws_secretsmanager_secret_version" "secret_version" {
-  secret_id = aws_secretsmanager_secret.secret.id
-  secret_string = jsonencode({
-    ACCESS_TOKEN               = random_password.access_token.result,
-    JWT_SECRET_KEY             = random_password.JWT_SECRET_KEY.result,
-    JWT_ACCESS_TOKEN_SECRET    = random_password.jwt_secret.result,
-    JWT_REFRESH_TOKEN_SECRET   = random_password.jwt_secret.result,
-    CLOUDFRONT_ID              = aws_cloudfront_distribution.s3_distribution.id,
-  })
+  # Update specific values in the secret
+  updated_secret = {
+    for key, value in local.existing_secret :
+    key => (
+      key == "ACCESS_TOKEN"    ? "random_password.access_token.result" :
+      key == "JWT_SECRET_KEY"      ? "random_password.JWT_SECRET_KEY.result"  :
+      key == "JWT_ACCESS_TOKEN_SECRET"    ? "random_password.JWT_ACCESS_REFRESH_TOKEN_SECRET.result" :
+      key == "JWT_REFRESH_TOKEN_SECRET"    ? "random_password.JWT_ACCESS_REFRESH_TOKEN_SECRET.result" :
+      value
+    )
+  }
 }
+
+# resource "aws_secretsmanager_secret_version" "secret_version" {
+#   secret_id = aws_secretsmanager_secret.secret.id
+#   secret_string = jsonencode({
+#     ACCESS_TOKEN               = random_password.access_token.result,
+#     JWT_SECRET_KEY             = random_password.JWT_SECRET_KEY.result,
+#     JWT_ACCESS_TOKEN_SECRET    = random_password.JWT_ACCESS_REFRESH_TOKEN_SECRET.result,
+#     JWT_REFRESH_TOKEN_SECRET   = random_password.JWT_ACCESS_REFRESH_TOKEN_SECRET.result,
+#   })
+# }
 
 #Random secrets generator 
 resource "random_password" "access_token" {
   length  = 32
   special = false
-}
-
-resource "random_password" "jwt_secret" {
-  length  = 256
-  special = false
+  upper   = true
+  lower   = true
+  numeric = true
 }
 
 resource "random_password" "JWT_SECRET_KEY" {
   length  = 256
   special = false
+  upper   = true
+  lower   = true
+  numeric = true
 }
 
-# AWS Secrets Manager Secret name random creation
-resource "random_string" "secret_suffix" {
-  length  = 8
+resource "random_password" "JWT_ACCESS_REFRESH_TOKEN_SECRET" {
+  length  = 256
   special = false
-} *)
+  upper   = true
+  lower   = true
+  numeric = true
+}
+
+
+# AWS Secrets Manager Secret name random creation
+# resource "random_string" "secret_suffix" {
+#   length  = 8
+#   special = false
+# }
