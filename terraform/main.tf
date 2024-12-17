92fc501707fb95f9ef704ae0a56309f06d679b40
MohamedRamadan2024 <mohamed.ramadan.bakry@vultara.com>
2024-12-17 20:17:49 +0200

    create secret

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,11 +16,11 @@ resource "aws_secretsmanager_secret_version" "new_secret_version" {
 locals {
   existing_secret = jsondecode(data.aws_secretsmanager_secret_version.old_secret.secret_string)
   updated_secret = merge(
-    local.existing_secret,
+    # local.existing_secret,
   # Update specific values in the secret
   {
-    "CVE_API_KEY" = contains(keys(local.existing_secret), "${local.existing_secret["CVE_API_KEY"]}"),
-    "ATLASDB_ALGOREAD" = contains(keys(local.existing_secret), "${local.existing_secret["ATLASDB_ALGOREAD"]}"),
+    "CVE_API_KEY" = lookup(local.existing_secret, "CVE_API_KEY", null),
+    "ATLASDB_ALGOREAD" = lookup(local.existing_secret, "CVE_API_KEY", null),
     "ACCESS_TOKEN" = contains(keys(local.existing_secret), "${random_password.access_token.result}") ? "${random_password.access_token.result}" : "${random_password.access_token.result}",
     "JWT_SECRET_KEY" = contains(keys(local.existing_secret), "${random_password.JWT_SECRET_KEY.result}") ? "${random_password.JWT_SECRET_KEY.result}" : "${random_password.JWT_SECRET_KEY.result}",
     "JWT_ACCESS_TOKEN_SECRET" = contains(keys(local.existing_secret), "${random_password.JWT_ACCESS_REFRESH_TOKEN_SECRET.result}") ? "${random_password.JWT_ACCESS_REFRESH_TOKEN_SECRET.result}" : "${random_password.JWT_ACCESS_REFRESH_TOKEN_SECRET.result}",
