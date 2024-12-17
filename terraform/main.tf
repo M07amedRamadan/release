5aa82f6431fc2097948db1be8a70ef4d149ddb98
MohamedRamadan2024 <mohamed.ramadan.bakry@vultara.com>
2024-12-17 20:01:14 +0200

    creating new one with only desired strings

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,6 +19,7 @@ locals {
     local.existing_secret,
   # Update specific values in the secret
   {
+    "CVE_API_KEY" = contains(keys(local.existing_secret), "${value}"),
     "ACCESS_TOKEN" = contains(keys(local.existing_secret), "${random_password.access_token.result}") ? "${random_password.access_token.result}" : "${random_password.access_token.result}",
     "JWT_SECRET_KEY" = contains(keys(local.existing_secret), "${random_password.JWT_SECRET_KEY.result}") ? "${random_password.JWT_SECRET_KEY.result}" : "${random_password.JWT_SECRET_KEY.result}",
     "JWT_ACCESS_TOKEN_SECRET" = contains(keys(local.existing_secret), "${random_password.JWT_ACCESS_REFRESH_TOKEN_SECRET.result}") ? "${random_password.JWT_ACCESS_REFRESH_TOKEN_SECRET.result}" : "${random_password.JWT_ACCESS_REFRESH_TOKEN_SECRET.result}",
@@ -26,35 +27,6 @@ locals {
   }
   )
 }
-
-# locals {
-#   existing_secret = jsondecode(data.aws_secretsmanager_secret_version.old_secret.secret_string)
-
-#   # Update specific values in the secret
-#   updated_secret = {
-#     "EXISTING_STRING_KEY" = "${local.existing_secret["EXISTING_STRING_KEY"]} new_value_to_append",
-
-#     for key, value in local.existing_secret :
-#     key => (
-#       key == "ACCESS_TOKEN"    ? random_password.access_token.result :
-#       key == "JWT_SECRET_KEY"      ? random_password.JWT_SECRET_KEY.result  :
-#       key == "JWT_ACCESS_TOKEN_SECRET"    ? random_password.JWT_ACCESS_REFRESH_TOKEN_SECRET.result :
-#       key == "JWT_REFRESH_TOKEN_SECRET"    ? random_password.JWT_ACCESS_REFRESH_TOKEN_SECRET.result :
-#       value
-#     )
-#   }
-# }
-
-
-# resource "aws_secretsmanager_secret_version" "secret_version" {
-#   secret_id = aws_secretsmanager_secret.secret.id
-#   secret_string = jsonencode({
-#     ACCESS_TOKEN               = random_password.access_token.result,
-#     JWT_SECRET_KEY             = random_password.JWT_SECRET_KEY.result,
-#     JWT_ACCESS_TOKEN_SECRET    = random_password.JWT_ACCESS_REFRESH_TOKEN_SECRET.result,
-#     JWT_REFRESH_TOKEN_SECRET   = random_password.JWT_ACCESS_REFRESH_TOKEN_SECRET.result,
-#   })
-# }
 
 #Random secrets generator 
 resource "random_password" "access_token" {
@@ -80,10 +52,3 @@ resource "random_password" "JWT_ACCESS_REFRESH_TOKEN_SECRET" {
   lower   = true
   numeric = true
 }
-
-
-# AWS Secrets Manager Secret name random creation
-# resource "random_string" "secret_suffix" {
-#   length  = 8
-#   special = false
-# }
