8136d2239d2241b4f8cec2097cf19eb27fffba25
MohamedRamadan2024 <mohamed.ramadan.bakry@vultara.com>
2024-12-17 14:15:49 +0200

    rename folder

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,3 @@
-variable "public_key" {
-  description = "The public API key for MongoDB Atlas"
-  type        = string
-}
-variable "private_key" {
-  description = "The private API key for MongoDB Atlas"
-  type        = string
-}
 
 variable "region" {
   description = "AWS Region"
