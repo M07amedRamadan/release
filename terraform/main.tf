dcec5f2f17515377a0ac35cc9e1536497a597983
M07amedRamadan <mohamed.ramadan.b0@gmail.com>
2024-12-17 16:12:38 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,39 +27,10 @@ locals {
   )
 }
 
-# locals {
-#   existing_secret = jsondecode(data.aws_secretsmanager_secret_version.old_secret.secret_string)
-
-#   # Update specific values in the secret
-#   updated_secret = {
-#     "EXISTING_STRING_KEY" = "${local.existing_secret["EXISTING_STRING_KEY"]} new_value_to_append",
-
-#     for key, value in local.existing_secret :
-#     key => (
-#       key == "ACCESS_TOKEN"    ? random_password.access_token.result :
-#       key == "JWT_SECRET_KEY"      ? random_password.JWT_SECRET_KEY.result  :
-#       key == "JWT_ACCESS_TOKEN_SECRET"    ? random_password.JWT_ACCESS_REFRESH_TOKEN_SECRET.result :
-#       key == "JWT_REFRESH_TOKEN_SECRET"    ? random_password.JWT_ACCESS_REFRESH_TOKEN_SECRET.result :
-#       value
-#     )
-#   }
-# }
-
-
-# resource "aws_secretsmanager_secret_version" "secret_version" {
-#   secret_id = aws_secretsmanager_secret.secret.id
-#   secret_string = jsonencode({
-#     ACCESS_TOKEN               = random_password.access_token.result,
-#     JWT_SECRET_KEY             = random_password.JWT_SECRET_KEY.result,
-#     JWT_ACCESS_TOKEN_SECRET    = random_password.JWT_ACCESS_REFRESH_TOKEN_SECRET.result,
-#     JWT_REFRESH_TOKEN_SECRET   = random_password.JWT_ACCESS_REFRESH_TOKEN_SECRET.result,
-#   })
-# }
-
 #Random secrets generator 
 resource "random_password" "access_token" {
   length  = 32
-  special = false
+  special = true
   upper   = true
   lower   = true
   numeric = true
@@ -67,7 +38,7 @@ resource "random_password" "access_token" {
 
 resource "random_password" "JWT_SECRET_KEY" {
   length  = 256
-  special = false
+  special = true
   upper   = true
   lower   = true
   numeric = true
@@ -75,15 +46,8 @@ resource "random_password" "JWT_SECRET_KEY" {
 
 resource "random_password" "JWT_ACCESS_REFRESH_TOKEN_SECRET" {
   length  = 256
-  special = false
+  special = true
   upper   = true
   lower   = true
   numeric = true
 }
-
-
-# AWS Secrets Manager Secret name random creation
-# resource "random_string" "secret_suffix" {
-#   length  = 8
-#   special = false
-# }
