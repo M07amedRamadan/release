b1e268c960e2f72d8436f91f6d169585867f47d9
MohamedRamadan2024 <mohamed.ramadan.bakry@vultara.com>
2024-12-19 15:40:56 +0200

    test running double resources depends on the input deployment

---
+++ terraform/CNAME_record.tf
@@ -0,0 +1,11 @@
+# resource "aws_route53_record" "cname_record" {
+#   zone_id = data.aws_route53_zone.vultara_zone.zone_id
+#   name    = "${var.CUSTOMER_NAME}.vultara.com"
+#   type    = "CNAME"
+#   ttl     = 3600
+#   records = [aws_cloudfront_distribution.s3_distribution.domain_name]
+# }
+
+# data "aws_route53_zone" "vultara_zone" {
+#   name = "vultara.com"  # Replace with the actual domain name
+# }
---
+++ terraform/s3_creation.tf
@@ -0,0 +1,67 @@
+# No change required in this file
+
+resource "aws_s3_bucket" "New_Customer_Bucket" {
+  for_each = toset(local.resource_names)
+  #bucket = "${var.CUSTOMER_NAME}.vultara.com"
+  force_destroy = true
+}
+
+#Public Access
+resource "aws_s3_bucket_public_access_block" "New_Customer_Bucket_Permissions_Block" {
+  bucket = aws_s3_bucket.New_Customer_Bucket.bucket
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+#########################################s3 report bucket#######################################
+
+# resource "aws_s3_bucket" "reports-bucket" {
+#   bucket = "${var.CUSTOMER_NAME}-reports-bucket"
+#   force_destroy = true
+# }
+
+# resource "aws_s3_bucket_ownership_controls" "ownership-reports-bucket" {
+#   bucket = aws_s3_bucket.reports-bucket.id
+#   rule {
+#     object_ownership = "ObjectWriter"
+#   }
+# }
+
+# resource "aws_s3_bucket_acl" "acl-reports-bucket" {
+#   depends_on = [aws_s3_bucket_ownership_controls.ownership-reports-bucket]
+
+#   bucket = aws_s3_bucket.reports-bucket.id
+#   acl    = "private"
+# }
+
+# resource "aws_s3_bucket_lifecycle_configuration" "lifecycle-reports-bucket" {
+   
+#   bucket = aws_s3_bucket.reports-bucket.id
+#   rule {
+#     id      = "Expire current versions of objects After 7 Days"
+#     status  = "Enabled"
+
+#     expiration {
+#       days = 7
+#     }
+#   }
+# }
+
+#####################################import bucket##################################################
+# resource "aws_s3_bucket" "import-bucket" {
+#   bucket = "${var.CUSTOMER_NAME}-import-bucket"
+#   force_destroy = true
+# } 
+
+# #still, the import bucket is private but can be accessed through the policy only.
+# resource "aws_s3_bucket_public_access_block" "Block-public-access" {
+#   bucket = aws_s3_bucket.import-bucket.id
+
+#   block_public_acls       = false
+#   block_public_policy     = false
+#   ignore_public_acls      = true
+#   restrict_public_buckets = true
+# }
