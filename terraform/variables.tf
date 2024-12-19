8665f756cd330026145749b3155a0c7941f932fc
MohamedRamadan2024 <mohamed.ramadan.bakry@vultara.com>
2024-12-19 22:34:06 +0200

    test creation double S3

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -16,5 +16,5 @@ variable "Application_type" {
 locals {
     resources= var.Application_type == "Vultara" ? ["${var.CUSTOMER_NAME}"] : var.Application_type == "SOC" ? ["${var.CUSTOMER_NAME}.soc"] : ["${var.CUSTOMER_NAME}","${var.CUSTOMER_NAME}.soc"]
     certificationArn= var.Application_type == "Vultara" ? ["arn:aws:acm:us-east-1:837491041518:certificate/74e193b1-9bae-49cd-af83-bc3f05ccbba1"] : var.Application_type == "SOC" ? ["arn:aws:acm:us-east-1:837491041518:certificate/dc1be4c8-8eb8-4d9f-9631-3d7dab4838a9"] : ["arn:aws:acm:us-east-1:837491041518:certificate/74e193b1-9bae-49cd-af83-bc3f05ccbba1","arn:aws:acm:us-east-1:837491041518:certificate/dc1be4c8-8eb8-4d9f-9631-3d7dab4838a9"]
-
+    
  }
