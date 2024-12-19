421e697c12b43fadeb24eadfeea6dfaea1ca1a09
MohamedRamadan2024 <mohamed.ramadan.bakry@vultara.com>
2024-12-19 22:21:33 +0200

    test creation double S3

--- terraform/CNAME_record.tf
+++ terraform/CNAME_record.tf
@@ -2,10 +2,10 @@ resource "aws_route53_record" "cname_record" {
   for_each = toset(local.resources)
   zone_id = data.aws_route53_zone.vultara_zone.zone_id
 # bucket = each.value "${var.CUSTOMER_NAME}.vultara.com"
-  name    = "${each.value}.vultara.com"
+  name    = "${each.value}"
   type    = "CNAME"
   ttl     = 3600
-  records = ["${aws_cloudfront_distribution.CloudFront[each.key].domain_name}"]
+  records = [aws_cloudfront_distribution.s3_distribution[each.key].domain_name]
 }
 
 data "aws_route53_zone" "vultara_zone" {
