2ba1aebc313b8f34c7431c2acae75b75615f876d
MohamedRamadan2024 <mohamed.ramadan.bakry@vultara.com>
2024-12-21 00:39:12 +0200

    update locals for other resources

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -6,10 +6,5 @@ locals {
       "${var.CUSTOMER_NAME}" = "arn:aws:acm:us-east-1:837491041518:certificate/74e193b1-9bae-49cd-af83-bc3f05ccbba1"
       "${var.CUSTOMER_NAME}.soc" = "arn:aws:acm:us-east-1:837491041518:certificate/dc1be4c8-8eb8-4d9f-9631-3d7dab4838a9"
     }
-    
-    countNum= {
-       "${var.CUSTOMER_NAME}" = "0"
-       "${var.CUSTOMER_NAME}.soc" = "1"
-    }
 
  }
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,23 @@
 # No change required in this file
 
 resource "aws_vpc" "New_Customer_VPC" {
-  for_each = toset(local.resources)
-  count = local.countNum[each.key]
-  
+  count         = var.Application_type == "Vultara" ? 1 : 0
   cidr_block           = "10.33.0.0/24"
   instance_tenancy     = "default"
   enable_dns_hostnames = true
 
   tags = {
     Name = "${var.CUSTOMER_NAME}-VPC-prod"
+  }
+}
+
+resource "aws_subnet" "public_1" {
+  count         = var.Application_type == "Vultara" ? 1 : 0
+  vpc_id        = aws_vpc.New_Customer_VPC.id
+  cidr_block    = cidrsubnet(aws_vpc.New_Customer_VPC.cidr_block, 3, 0) # Index 0
+  availability_zone = "${var.region}c"
+
+  tags = {
+    Name = "${var.CUSTOMER_NAME}-public-sub-1"
   }
 }
